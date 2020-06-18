586ecdc4c4dd41221d5b89b0ce7e7ea9f1d52dd2
smsk <mohammadsadeghkeshavarzi@yahoo.com>
2020-06-18 13:31:45 +0430

    Add tests from Quera

--- t.py
+++ t.py
@@ -2,7 +2,7 @@ from lark import Lark
 
 g = ''' start: IDENT | BOOL
         IDENT :  /(?!((true)|(false)|(void)|(int)|(double)|(bool)|(string)|(class)|(interface)|(null)|(this)|(extends)|(implements)|(for)|(while)|(if)|(else)|(return)|(break)|(new)|(NewArray)|(Print)|(ReadInteger)|(ReadLine))([^_a-zA-Z0-9]|$))[a-zA-Z][_a-zA-Z0-9]*/
-        BOOL : /((true)|(false))(xxxxxxxxx)*/
+        BOOL : /((true)|(false))(xabc1235ll)*/
 '''
 
 print(Lark(grammar=g, parser='lalr').parse(input()))
