7ef9fed1b7161df33321c5756ea1dc0c3592b0b2
sepehrMSP <sepehrpourghannad@yahoo.com>
2020-07-25 18:16:54 +0430

    Add some examples to get start with Lark

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
-from lark import Lark, Transformer
+from lark import Lark, Transformer, Tree
+from lark.visitors import Interpreter
 from lark.lexer import Lexer, Token
 
 
 class T(Transformer):
 
     def INT(self, tok):
-        "Convert the value of `tok` from string to int, while maintaining line number & column."
         return tok.update(value=int(tok))
 
 
@@ -21,17 +21,6 @@ IF: "if"
 %import common.INT
 %ignore " "
 """, parser="lalr")
-
-parser2 = Lark(r"""
-        start: _NL? section+
-        section: "[" NAME "]" _NL item+
-        item: NAME "=" VALUE? _NL
-        VALUE: /./+
-        %import common.CNAME -> NAME
-        %import common.NEWLINE -> _NL
-        %import common.WS_INLINE
-        %ignore WS_INLINE
-    """, parser="lalr")
 
 sample_conf = """
 [bla]
@@ -111,6 +100,10 @@ THEN : "then"
 
 
 class TestTransformer(Transformer):
+    # def start(self, token):
+    #     print(token , type(token))
+    #     return token
+
     def navid(self, token):
         print()
         print(token)
@@ -119,11 +112,59 @@ class TestTransformer(Transformer):
 
     def BOOL(self, token):
         print(token + "   salam")
+        print(type(token))
         return token
+
+
+class NewTransformer(Transformer):
+    def start(self, dard):
+        # print(dard.meta)
+        print(dard[0].meta)
+        return Tree("start", dard)
+
+    def section(self, kooft):
+        print("section")
+        print(kooft)
+        print(type(kooft))
+        print(self.__repr__())
+        kooft[0].value = 111
+        return Tree("section", kooft, [12, 2])
+
+    def item(self, maraz):
+        print("item")
+        print(maraz)
+        return maraz
 
 
 test_parser = Lark(test_grammar, parser="lalr", transformer=TestTransformer())
 test_parser2 = Lark(test_grammar2, parser="lalr")
+parser2 = Lark(r"""
+        start: _NL? section+
+        section: "[" NAME "]" _NL item+
+        item: NAME "=" VALUE? _NL -> kk1 
+            | NAME "****" -> kk2
+        VALUE: /./+
+        %import common.CNAME -> NAME
+        %import common.NEWLINE -> _NL
+        %import common.WS_INLINE
+        %ignore WS_INLINE
+    """, parser="lalr", transformer=NewTransformer())
+
+
+class TestInterpreter(Interpreter):
+    def start(self, tree):
+        print("oomadim too")
+        print(tree.data)
+        print(tree)
+        # self.visit_children(tree.children)
+        # self.visit(tree.children[0][1]) todo this works
+
+    def section(self, tree):
+        print("lamassab")
+
+    def kk1(self, tree):
+        print("baw too kk1 am")
+
 
 if __name__ == '__main__':
     try:
@@ -132,10 +173,13 @@ if __name__ == '__main__':
         print("tamoooooooooooooooooooooooooooooooooom")
 
     tri2 = parser2.parse(sample_conf)
+    print("\n\n\n")
+    TestInterpreter().visit(tri2)
     # print(tri.children)
-    print(tri2.data)
-    print(tri2.children)
-    print()
-    print()
+    # print(tri2.data)
+    # print(tri2.children)
+    # print()
+    # print()
+    print("\n\n\n")
 
     print(test_parser.parse('123  true how ever you far '))
