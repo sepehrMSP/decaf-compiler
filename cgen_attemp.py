e3f2ca4167f7b2f5a046424acdf24570fba4789f
SmsS4 <smss.lite@gmail.com>
2020-07-31 19:45:00 +0430

    Fix clear in run.py

--- cgen_attemp.py
+++ cgen_attemp.py
@@ -1336,12 +1336,22 @@ int main(){
 """
 if __name__ == '__main__':
     print(cgen("""
-    int main(){
-        Print(3.123456);
-    }
+int main(){
 
+    int i;
+    int j;
+    string [][] s;
 
+    s = NewArray(3, string[]);
+    // s[0] = NewArray(2, string);
+    s[1] = NewArray(2, string);
+    // s[2] = NewArray(2, string);
 
+    // s[0][0] = "a00";
+    // s[0][1] = "a01";
+    a[1][0] = "a10";
+    // a[1][0] = "a10"; a[1][1] = "a11"; a[2][0] = "a20"; a[2][1] = "a21"; for(i=0; i < 3; i = i+1){ for(j=0; j < 2; j = j+1){ Print(s[i][j]); } }
+}
     """))
 
     exit(0)
