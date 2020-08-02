0ae55092cf0966e9ab704cd217cf5e939d80c443
SmsS4 <smss.lite@gmail.com>
2020-08-03 01:31:36 +0430

    Write main.py

--- phase3_tests/main.py
+++ phase3_tests/main.py
@@ -1,37 +1,27 @@
 import sys, getopt
+from cgen_attemp import cgen
+
+
 def main(argv):
     inputfile = ''
     outputfile = ''
     try:
-        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+        opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
     except getopt.GetoptError:
-        print ('main.py -i <inputfile> -o <outputfile>')
+        print('main.py -i <inputfile> -o <outputfile>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print ('test.py -i <inputfile> -o <outputfile>')
+            print('test.py -i <inputfile> -o <outputfile>')
             sys.exit()
         elif opt in ("-i", "--ifile"):
             inputfile = arg
         elif opt in ("-o", "--ofile"):
             outputfile = arg
 
-    with open("tests/" + inputfile, "r") as input_file:
-        # do stuff with input file
-        pass
-
     with open("out/" + outputfile, "w") as output_file:
-        # write result to output file. 
-        # for the sake of testing :
-        output_file.write("""
-# PROGRAM: Hello, World!
-.data # Data declaration section
-out_string: .asciiz "Hello, World!"
-.text # Assembly language instructions
-main: # Start of code section
-li $v0, 4 # system call code for printing string = 4
-la $a0, out_string # load address of string to be printed into $a0
-syscall # call operating system to perform operation in $""")
+        with open("tests/" + inputfile, "r") as input_file:
+            output_file.write(cgen(''.join(input_file.readlines())))
 
 
 if __name__ == "__main__":
