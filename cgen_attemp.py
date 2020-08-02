f45230ae6298b92a2d7ba85341cdb670b796e75d
smsk <mohammadsadeghkeshavarzi@yahoo.com>
2020-08-02 18:59:22 +0430

    Add class tree builder to cgen

--- cgen_attemp.py
+++ cgen_attemp.py
@@ -2134,6 +2134,8 @@ def cgen(decaf):
     parser = Lark(grammar, parser="lalr")
     parse_tree = parser.parse(decaf)
     SymbolTableMaker().visit(parse_tree)
+    ClassTreeSetter().visit(parse_tree)
+    set_inheritance()
     ImplicitThis().visit(parse_tree)
     # print(symbol_table)
     return CodeGenerator().visit(parse_tree)
@@ -2229,166 +2231,50 @@ int main() {
 """
 
 decaf = """
+//
+// Classes with inheritance
+//
+
 class Animal {
-    string name;
-    int age;
+  int height;
+  Animal mother;
+  void InitAnimal(int h, Animal mom) {
+    this.height = h;
+    mother = mom;
+  }
 
-    void set_name(string name2) {
-        name = name2;
-    }
+  int GetHeight() {
+    return height;
+  }
 
-    string get_name(){
-        return this.name;
-    }
-
-    void init_age() {
-        age = 0;
-    }
-
-    void add_age(int years) {
-        age = age + years;
-    }
-
-    int get_age(){
-        return age;
-    }
-
-     string battle(Animal b) {
-        return "WRONG FIGHT";
-     }
-
-    void get_voice() {
-        Print("....");
-    }
-
-    void init_power() {}
-    int get_power() {}
-    void add_power(int d) {}
+  Animal GetMom() {
+    return this.mother;
+  }
 }
 
-class WildAnimal extends Animal {
-    int power;
-    void init_power() {
-        power = 0;
-    }
-
-    void add_power (int d ){
-        power = power + d;
-    }
-
-    int get_power() {
-        return power;
-    }
-
-    string battle(Animal oth) {
-        return "OKAY";
-    }
+class Cow extends Animal {
+  bool isSpotted;
+  void InitCow(int h, Animal m) {
+    isSpotted = true;
+    this.InitAnimal(h, m);
+  }
+  bool IsSpottedCow () {
+    return isSpotted;
+  }
 }
 
-class FarmAnimal extends Animal {
-    int power;
-    void init_power() {
-        power = 0;
-    }
 
-    void add_power (int d ){
-        power = power + d;
-    }
+void main() {
+  Cow betsy;
+  Animal b;
+  betsy = new Cow;
 
-    int get_power() {
-        return power;
-    }
+  betsy.InitCow(5, null);
+  b = betsy;
+  b.GetMom();
+  Print("spots: ",betsy.IsSpottedCow(), "    height: ", b.GetHeight());
 }
 
-class Lion extends WildAnimal {
-    void get_voice() {
-        Print("LION");
-    }
-}
-
-class Tiger extends WildAnimal {
-    void get_voice() {
-        Print("TIGER");
-    }
-}
-
-class Sheep extends FarmAnimal {
-    void get_voice() {
-        Print("SHEEP");
-    }
-}
-
-int main() {
-    Animal[] animals;
-    int n;
-    int years;
-    int i;
-    int query;
-
-    Print("Num of animals");
-    n = ReadInteger();
-
-    animals = NewArray(n, Animal);
-
-    for (i=0; i<n; i=i+1) {
-string type;
-        string name;
-
-        Print("type and name of animal ", i);
-
-        type = ReadLine();
-        if (type == "L") {
-            animals[i] = new Lion;
-            //animals[i].init_power();
-        } else if(type == "T") {
-            animals[i] = new Tiger;
-            //animals[i].init_power();
-        }
-        else {
-            animals[i] = new Sheep;
-            //animals[i].init_defence();
-        }
-
-        animals[i].init_age();
-        name = ReadLine();
-        animals[i].set_name(name);
-        animals[i].init_power();
-    }
-
-    for (i=0; i<n; i=i+1) {
-        animals[i].get_voice();
-        Print(animals[i].get_name());
-    }
-
-    Print("Years :");
-
-    years = ReadInteger();
-    for (i=0; i<n; i=i+1) {
-        animals[i].add_age(i * years);
-    }
-
-    Print("Queries :");
-
-    query = ReadInteger();
-    for (i=0; i<query; i=i+1) {
-        int d;
-        int id;
-        Print("id and d");
-        id = ReadInteger();
-        d = ReadInteger();
-        animals[id].add_power(d);
-    }
-
-    Print("Fights :");
-    query = ReadInteger();
-    for (i=0; i<query; i=i+1) {
-        int i1;
-        int i2;
-        i1 = ReadInteger();
-        i2 = ReadInteger();
-        Print(animals[i1].battle(animals[i2]));
-    }
-}
 """
 
 if __name__ == '__main__':
