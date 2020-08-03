3f478ad8a6400891ab781fa65f8b3956bcfbb219
SmsS4 <smss.lite@gmail.com>
2020-08-03 21:13:52 +0430

    Fix run.sh

--- phase3_tests/run.sh
+++ phase3_tests/run.sh
@@ -3,7 +3,7 @@ mkdir -p "out"
 mkdir -p "report"
 cd "./tests"
 prefix="t" ;
-dirlist=(`ls gi*.d`) ;
+dirlist=(`ls *.d`) ;
 OUTPUT_DIRECTORY="out/"
 TEST_DIRECTORY="tests/"
 REPORT_DIRECTORY="report/"
